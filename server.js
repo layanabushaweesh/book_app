be4a3141ad87674b54b0a1d1548af20c3ad12f62
layanabushaweesh <layanabushaweesh@gmail.com>
2021-04-05 20:53:17 +0300

    first update

--- server.js
+++ server.js
@@ -4,29 +4,39 @@
 require('dotenv').config();
 const express = require('express');
 const superagent = require('superagent');
-// const pg = require('pg');
+const pg = require('pg');
 const cors = require('cors');
-// const path = require('path');
-// const { response } = require('express');
+
 
 // App Setup
 const app = express();
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+
 
 // App Middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
-// Setting View Engine
-// app.set('views', path.join(__dirname), 'views');
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public')); // for css
+app.use(express.static(__dirname + '/public')); 
 
 
 // API Routes
 app.get('/', renderHome);
+
 app.get('/searches/new', renderSearch);
 app.post('/searches', renderResults);
 
+//rout when i select the id of the bok
+
+app.get('/books/:id', renderIdBook);
 
 
 
@@ -40,9 +50,28 @@ app.listen(PORT, () => console.log(`Up listen on PORT ${PORT}`));
 
 
 // Render Functions
+//1 refactor / rout
 function renderHome(req, res) {
-  res.render('pages/index');
+  
+  let databaseRowCount = 0;
+  //The COUNT(*) function returns the number of records in a table:
+  const sqlQ = 'SELECT COUNT(*) FROM books'
+  client.query(sqlQ).then(data => {
+    // console.log(data.rowCount);
+    databaseRowCount = data.rowCount;
+  })
+
+  const sqlQuery = 'SELECT * FROM books';
+  client.query(sqlQuery).then(data => {
+    res.render('pages/index', { 
+      databaseResults: data.rows,
+      rowCount: databaseRowCount
+    });
+  });
+  
 }
+
+
 
 function renderSearch(req, res) {
   res.render('pages/searches/new');
