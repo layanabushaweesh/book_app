ee3962971d7f5653890c44458a17d866a0eb07d3
layanabushaweesh <layanabushaweesh@gmail.com>
2021-04-08 08:08:25 +0300

    up

--- server.js
+++ server.js
@@ -1,129 +1,162 @@
-'use strcit'
-
-// Load environment variables module
+'use strict';
+// Dependencies
 require('dotenv').config();
-
-// Load modules into our script 
 const express = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
 const cors = require('cors');
-
-const DATABASE_URL = process.env.DATABASE_URL;
-const NODE_ENV = process.env.NODE_ENV;
-
-const options = NODE_ENV === 'production' ? { connectionString: DATABASE_URL, ssl: { rejectUnauthorized: false } } : { connectionString: DATABASE_URL };
-// App setup 
+const pg = require('pg');
+const methodOverride = require('method-override');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const client = new pg.Client(options);
-client.on('error', (err) => console.log(err));
-app.use(cors()); //will respond to any request and allow access to our api from another domain
+
+// Setup environment
+const PORT = process.env.PORT || 3030;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// Middleware
+app.use(cors());
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/public', express.static('public'));
-app.set('view engine', 'ejs'); // Set the view engine for server-side 
 
-app.get('/searches/new', showDetalis);
+// database Setup
+const client =  new pg.Client({
+  connectionString: DATABASE_URL,
+});
 
-// Creates a new search to the Google Books API
-app.post('/searches', serch);
+// HomePage
+const home = (req, res) => {
+  const select = 'select * from books ;';
+  client.query(select).then((data) =>{
+    res.render('pages/index', {books : data.rows , count: data.rows.length});
+  }).catch((err) => errorHandler(err, req, res));
+};
 
-app.get('*', (req, res) => res.status(404).send(' not exist'));
+// Just a test
+app.get('/hello', (req, res) => {
+  res.render('pages/index');
+});
 
-function showDetalis(req, res) {
-    res.render('pages/searches/new')
+// Search for a book by title 
+const renderSearch = (req, res) => {
+  res.render('pages/searches/new');
+};
+
+// Show Search Results
+const renderSearchResults = (req, res) => {
+  const searchKeyword = req.body.searched;
+  const searchBy = req.body.searchBy;
+  // let url = `https://www.googleapis.com/books/v1/volumes?q=${req.body.searchQuery}+${req.body.searchBy === 'title' ? 'intitle' : 'inauthor'}`;
+  const url = `https://www.googleapis.com/books/v1/volumes?langRestrict=en&q=${searchKeyword}+in${searchBy}:`;
+
+  superagent.get(url).then((data) => {
+    const bookData = data.body.items;
+    const book = bookData.map(item => {
+      return new Book(item.volumeInfo );
+    });
+    res.render('pages/searches/show', { books: book });
+  }).catch((err) => errorHandler(err, req, res));
+};
+
+// Adding  to The Home Page
+const addBook = (req,res) => {
+  let id ;
+  let SQL = 'INSERT INTO books (title, author, description, image_url , offShelf ,isbn) VALUES ($1,$2,$3,$4,$5,$6) RETURNING ID;';
+  const { title, author, description, image_url , offShelf , isbn } = req.body;
+  const values = [title, author, description, image_url , offShelf ,isbn ];
+  const sqlSearch = `SELECT * FROM books WHERE isbn = '${isbn}' ;`;
+  client.query(sqlSearch).then((searchedResult) => {
+    if (searchedResult.rowCount > 0) {
+      res.redirect(`/books/${searchedResult.rows[0].id}`);
+    }
+    else{
+      client.query(SQL,values)
+        .then ((result) => {
+          id =result.rows[0].id ;
+          res.redirect(`/books/${id}`);
+        })
+        .catch((err) => {
+          errorHandler(err, req, res);
+        });
+    }
+  });
+};
+
+// Single Book 
+const showBook = (req,res) => {
+  const SQL = `SELECT * from books WHERE ID=${req.params.ID};`;
+  client.query(SQL)
+    .then(result => {
+      console.log(' result.rows[0]', result.rows[0]);
+      res.render('pages/books/show', { books: result.rows[0]});
+    }).catch((err) => errorHandler(err, req, res));
+};
+
+
+// Update The Database
+const updateBook = (req, res) => {
+  const id = req.params.ID ;
+  console.log('req.body', req.body);
+  console.log('req.params.id' , id);
+  const setSQL = `UPDATE books SET title=$1, author=$2, description=$3 , offShelf=$4, isbn=$5
+  WHERE id=$6;`;
+  const { title, author, description , offShelf , isbn } = req.body;
+  const values = [title, author, description, offShelf ,isbn , id];
+  client.query(setSQL, values)
+    .then(() => {
+      res.redirect(`/books/${id}`);
+    }).catch((err) => errorHandler(err, req, res));
+};
+
+function deleteBook(req, res) {
+  const id = req.params.ID ;
+  const deleteSQL = `DELETE FROM books WHERE ID = ${id};`;
+  client.query(deleteSQL)
+    .then(() => {
+      res.redirect(`/books/${id}`);
+    }).catch((err) => errorHandler(err, req, res));
+
+
 }
 
-// console.log(superagent)
-// render the index.ejs from DB
+// wrong path rout
+const handelWrongPath = (err, req, res) => {
+  errorHandler(err, req ,res);
+};
 
-app.get('/', (req, res) => { 
-    const SQL = 'SELECT * FROM books;';
-    client
-        .query(SQL)
-        .then((results) => {
-            res.render('pages/index', { book: results.rows });
-        })
-        .catch((err) => {
-            errorHandler(err, req, res);
-        });
+// database connection
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log('connected to db', client.connectionParameters.database);
+    console.log(`The server is running on port ${PORT}`);
+  });
+}).catch(error => {
+  console.log('error', error);
 });
-// render the datails of a book
-
-app.get('/books/:id', (req, res) => { 
-    const SQL = 'SELECT * FROM books WHERE id=$1;';
-    const values = [req.params.id];
-    console.log(values);
-    client
-        .query(SQL, values)
-        .then((results) => {
-            // console.log(results)
-            res.render('pages/books/show', { book: results.rows[0] });
-        })
-        .catch((err) => {
-            errorHandler(err, req, res);
-        });
-});
-// Insert books into DB if not
-app.post('/books', (req, res) => { 
-
-    // let values = [req.body.isbn];
-    let SQL = 'INSERT INTO books (image_url,title,author,description) VALUES ($1,$2,$3,$4) RETURNING id ;';
-    let values = [req.body.img, req.body.title, req.body.author, req.body.description];
-    client.query(SQL, values).then((results) => {
-            // if (results.rows.length > 0) {
-            // res.redirect(`/books/${results.rows[0].id}`);
-            //  else {
-            res.redirect(`/books/${results.rows[0].id}`);
-            // console.log("inside insert")
-            // let sqlQuery = 'SELECT * FROM books'
-            // client.query(sqlQuery).then((results) => {
-            //         console.log('data returned back from db ', results);
-
-        })
-        .catch((err) => {
-            errorHandler(err, req, res);
-        })
-        // }
-});
-
-
-
-
-function serch(request, response) {
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  
-    request.body.radio === 'title' ? url += `intitle=${request.body.search}` : url += `inauthor=${request.body.search}`;
-    console.log(url);
-    
-
-    superagent.get(url)
-        .then(apiData => {
-            const Books = apiData.body.items.map(data => {
-                return new Book(data);
-            })
-            response.render('pages/searches/show', { book: Books })
-        })
-        .catch(err => errorHandler(err, request, response))
-}
-
-
-
 
 // constructor
-function Book(data) {
-    this.image_url = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
-    this.title = data.volumeInfo.title ? data.volumeInfo.title : "DEFULT TITLE";
-    this.author = data.volumeInfo.authors ? data.volumeInfo.authors : "DEFULT AUTHOR";
-    this.description = data.volumeInfo.description ? data.volumeInfo.description : "DEFULT DESCRIPTION";
-    // this.isbn = (data.volumeInfo.industryIdentifiers && data.volumeInfo.industryIdentifiers[0].identifier) ? data.volumeInfo.industryIdentifiers[0].identifier : "NO ISBN AVAILABLE"
+function Book(data ) {
+  this.title = (data.title)? data.title : 'Unknown Book Title';
+  this.author = (data.authors)? data.authors : 'Unknown Book Authors';
+  this.description = (data.description)? data.description : 'Description not available';
+  this.thumbnail = (data.imageLinks.thumbnail) ? data.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
+  this.isbn = data.industryIdentifiers ? `${data.industryIdentifiers[0].type} ${data.industryIdentifiers[0].identifier}` : 'Unknown ISBN';
+  this.offShelf = (data.categories) ? data.categories : 'The book is not in a shelf';
+
 }
 
+
+// API home page Routes
+app.get('/', home);
+app.get('/searches/new', renderSearch);
+app.post('/searches', renderSearchResults);
+app.get('/books/:ID', showBook);
+app.post('/books', addBook);
+app.put('/books/:ID', updateBook);
+app.delete('/books/:ID', deleteBook);
+app.use('*',handelWrongPath);
+
+// Error Handler
 function errorHandler(err, req, res) {
-    res.status(500).send(err);
+  res.render('pages/error', { err :err.message});
 }
-
-client.connect()
-    .then(app.listen(PORT, () =>
-        console.log(`app is listening on ${PORT}`)))
